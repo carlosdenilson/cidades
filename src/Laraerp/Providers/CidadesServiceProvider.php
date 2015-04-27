0df609666959f1cfd2b001d43687ec9b9ee7fb07
Jansen Felipe <jansen.felipe@gmail.com>
2015-04-27 00:46:14 -0300

    First commit

---
+++ src/Laraerp/Providers/CidadesServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Laraerp\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CidadeServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot() {
+        $back = DIRECTORY_SEPARATOR . '..';
+        $database = __DIR__ . $back . $back . DIRECTORY_SEPARATOR . 'database' . DIRECTORY_SEPARATOR;
+
+        $this->publishes([
+            $database . 'migrations/create_cidades_table.php' => base_path('database/migrations').date('Y_m_d_His_').'create_cidades_table.php',
+            $database . 'seeds' => base_path('database/seeds'),
+        ]);
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+        // Bind any implementations.
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides() {
+        return [];
+    }
+
+}
+
